b21c8a36ea8611d35566d259155bc5abf184c7a1
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2019-08-23 15:36:19 +0200

    Travis reconfigured

--- generate_and_test.sh
+++ generate_and_test.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
+./generate_template.sh
+cd generated-project
+
 case $TEMPLATE in
     bobcat-bdd|bobcat-junit5)
-        ./generate_template.sh
-        cd generated-project
         ./gradlew clean test -Dwebdriver.type=remote -Dwebdriver.url=https://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub -Dwebdriver.cap.browserName=chrome -Dwebdriver.cap.os=WINDOWS
         ;;
     bobcat-appium)
-        ./generate_template.sh
-        cd generated-project
         ./gradlew clean test -Dwebdriver.type=ios -Dwebdriver.url=https://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub -Dwebdriver.cap.device="iPhone XR" -Dwebdriver.cap.os_version=12 -Dwebdriver.cap.realMobile=true
         ;;
     *)
-        ./generate_template.sh
-        cd generated-project
         ./gradlew clean assemble
         ;;
 esac
