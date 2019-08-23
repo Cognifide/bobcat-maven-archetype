8ac6057edcc945f271d2770fb780cac57fc1416b
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2019-08-23 15:25:54 +0200

    Travis reconfigured

--- generate_and_test.sh
+++ generate_and_test.sh
@@ -7,13 +7,13 @@ case $TEMPLATE in
         ./gradlew clean test -Dwebdriver.type=remote -Dwebdriver.url=https://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub -Dwebdriver.cap.browserName=chrome -Dwebdriver.cap.os=WINDOWS
         ;;
     bobcat-appium)
+        ./generate_template.sh
+        cd generated-project
         ./gradlew clean test -Dwebdriver.type=ios -Dwebdriver.url=https://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub -Dwebdriver.cap.device="iPhone XR" -Dwebdriver.cap.os_version=12 -Dwebdriver.cap.realMobile=true
         ;;
     *)
         ./generate_template.sh
         cd generated-project
-        ./gradlew clean assembly
+        ./gradlew clean assemble
         ;;
 esac
-
-generated-project/gradlew clean test
